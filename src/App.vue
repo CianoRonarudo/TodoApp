874175d443ea9c2435f4031003b26cad146e93a8
CianoRonarudo <sovidecanisius@gmail.com>
2024-07-06 17:39:04 +0100

    Mise en place de la base de données

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main> -->
-
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  
 
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+  
+const todos = [
+  {
+    id: 1,
+    name: 'Learn',
+    complete: true,
+    priority : 'High'
+  },
+  {
+    id: 2,
+    name: 'Eat',
+    complete: false,
+    priority : 'Low'
+  },
+  {
+    id: 3,
+    name: 'Pray',
+    complete: true,
+    priority : 'High'
+  },
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+]
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
